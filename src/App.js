df3095683795e6ac793f3916145e3c20d9b1dca8
ganeshsuryawansh <96677595+ganeshsuryawansh@users.noreply.github.com>
2022-07-11 22:51:41 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import TextForm from './components/TextForm';
 //import About from './components/About';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
-{/*import{
-  Route,
-  Routes, 
-} from'react-router-dom';*/}
+//{/*import{Route,Routes, } from'react-router-dom';*/}
 
 function App() {
   const [mode, setMode] = useState('light');//Whether dark mode is enabled or not
